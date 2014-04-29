8277df71d302095767528f0f0472eb0befb118c7
njiiriJohnii <njiirijohnie@yahoo.com>
2014-04-29 09:56:20 +0300

    google authentification

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qwert::Application.config.secret_key_base = '2c2916bdd4985cb23e684aaf1e3a989465e8439c590837cdd42e86461334346741dd67e3ba4efdc124282d90d3183ce3b220fd19adf57c4470c74a9394816d97'
